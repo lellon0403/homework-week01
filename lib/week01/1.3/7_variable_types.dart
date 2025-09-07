4e583c399daa97db7b26acbd7ef5cb3f0f2e5c26
lellon0403 <a67733198@gmail.com>
2025-09-07 22:44:35 +0900

    feat: Add variable types example and initial list type file.

---
+++ lib/week01/1.3/7_variable_types.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = '코드팩토리';
+
+  int isInt = 10;
+
+  double isDouble = 2.5;
+
+  bool isTrue = ture;
+
+  print(name);
+  print(isInt);
+  print(isDouble);
+  print(isTrue);
+}
