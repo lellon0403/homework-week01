fdf68224e66fe47dbec7a1fc9c6e37f29ba0a473
lellon0403 <a67733198@gmail.com>
2025-09-07 22:49:35 +0900

    feat: Add example of using the `where` function on a list.

---
+++ lib/week01/1.4.1/3_where_function.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
+
+  final newList = blackPinkList.where((name) => name == '리사' || name == '지수');
+
+  print(newList);
+  print(newList.toList());
+}
