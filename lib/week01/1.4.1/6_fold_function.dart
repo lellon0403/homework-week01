f9b9cf910b53f87f50916c64a844bd4ef665627f
lellon0403 <a67733198@gmail.com>
2025-09-07 22:56:05 +0900

    Fix: Correct typo and add type annotation to fold function

--- lib/week01/1.4.1/6_fold_function.dart
+++ lib/week01/1.4.1/6_fold_function.dart
@@ -2,5 +2,10 @@ void main() {
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
 
   //reduce () 함수와 마찬가지로 각 요소를 순회하며 실행됩니다.
-  final allMembers = blackPinkList.
+  final allMembers = blackPinkList.fold<int>(
+    0,
+    (value, element) => vlaue + element.length,
+  );
+
+  print(allMemvers);
 }
