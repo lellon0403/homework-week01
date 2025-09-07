c92372377fbca27e627f7e5963b5d028a2b18572
lellon0403 <a67733198@gmail.com>
2025-09-07 23:03:36 +0900

    Fix: Correct Map type declaration syntax

--- lib/week01/1.4.2/1_1_map_type.dart
+++ lib/week01/1.4.2/1_1_map_type.dart
@@ -1,12 +1,10 @@
-void main(){
+void main() {
+  Map<String, String> dictionary = {
+    'Harry Potter': '해리 포터',
+    'Ron Weasley': '론 위즐리',
+    'Hermione Granger': '헤르미온느 그레인저',
+  };
 
-  Map(String, String> dictionary = {
-    'Harry Potter' : '해리 포터',
-    'Ron Weasley' : '론 위즐리',
-    'Hermione Granger' : '헤르미온느 그레인저',};
-
-    print(dictionary['Harry Potter']);
-    print(dictionary['Hermione Granger']);
-
-
-  }
+  print(dictionary['Harry Potter']);
+  print(dictionary['Hermione Granger']);
+}
