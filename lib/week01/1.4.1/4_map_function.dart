3ced6355f9245d464f8ab5006e8f22cedc6d343c
lellon0403 <a67733198@gmail.com>
2025-09-07 22:52:35 +0900

    feat: Add map function example and introduce `toList()`

--- lib/week01/1.4.1/4_map_function.dart
+++ lib/week01/1.4.1/4_map_function.dart
@@ -1,7 +1,11 @@
 void main() {
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
 
-  final newBlack = blackPinkList.map((name) => '블랙핑크 &name');
+  final newBlackPink = blackPinkList.map((name) => '블랙핑크 &name');
 
   print(newBlackPink);
+
+  // Interable을 List로 변환하고 싶을때 .toList() 사용
+
+  print(newBlackPink.toList());
 }
