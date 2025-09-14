e2f2019e19745a526f90d372d0993b01db9a782b
lellon0403 <a67733198@gmail.com>
2025-09-14 12:31:11 +0900

    Fix: Correct typo and implement override in GirlGroup class

--- lib/week02/2.4/1_override.dart
+++ lib/week02/2.4/1_override.dart
@@ -13,7 +13,15 @@ class Idol {
   }
 }
 
-class GrilGroup extends Idol {
+class GirlGroup extends Idol {
   //2.3 상속에서처럼 super 키워드를 사용해도 되고 다음처렁 생성자의 매개변수로
   // 직접 super 키워드를 사용해도 됩니다.
+
+  GirlGroup(super.name, super.membersCount);
+
+  // override 키워드를 사용해 오버라이드합니다.
+  @override
+  void sayName() {
+    print('저는 여자아이돌 ${this.name}입니다.');
+  }
 }
