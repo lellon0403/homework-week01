3cb5eafcc7d7ec72723af06cdf4b2bc666807c2a
lellon0403 <a67733198@gmail.com>
2025-09-14 16:06:48 +0900

    feat(cascade): Implement basic cascade operator example

--- lib/week02/2.10/1_cascade_operator.dart
+++ lib/week02/2.10/1_cascade_operator.dart
@@ -4,11 +4,20 @@ class Idol {
 
   Idol(this.name, this.membersCount);
 
-  void sayName() {}
+  void sayName() {
+    print('저는 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명입니다.');
+  }
 }
 
 void main() {
   //cascade operator (...)을 사용하면
   // 선언한 변수의 메서드를 연속으로 실행할 수 있습니다.
   Idol blackpink = Idol('블랙핑크', 4);
+
+  ...sayName();
+  blackpink.sayMembersCount();
 }
