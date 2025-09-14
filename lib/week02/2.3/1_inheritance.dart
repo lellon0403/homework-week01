8248d50f23710399d8d838516815acbcb3542452
lellon0403 <a67733198@gmail.com>
2025-09-14 12:21:11 +0900

    feat(inheritance): Add sayMembersCount method to Idol class

--- lib/week02/2.3/1_inheritance.dart
+++ lib/week02/2.3/1_inheritance.dart
@@ -4,7 +4,11 @@ class Idol {
 
   Idol(this.name, this.membersCount);
 
-  void sayName(){
-    print('저는 $')
+  void sayName() {
+    print('저는 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명입니다.');
   }
 }
