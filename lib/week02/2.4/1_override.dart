ea14f9a542ad686ed79d65a93a32502af285cf6a
lellon0403 <a67733198@gmail.com>
2025-09-14 12:32:42 +0900

    feat(week02): Add example usage of method overriding

--- lib/week02/2.4/1_override.dart
+++ lib/week02/2.4/1_override.dart
@@ -25,3 +25,14 @@ class GirlGroup extends Idol {
     print('저는 여자아이돌 ${this.name}입니다.');
   }
 }
+
+void main() {
+  GirlGroup blackPink = GirlGroup('블랙핑크', 4);
+
+  blackPink.sayName(); // 자식 클래스의 오버라이드된 메서드 사용
+
+  //sayMembersCount는 오버라이드하지 않았기 때문에
+  //그대로 Idol 클래스의 메서드가 실행됩니다.
+  //부모 클래스의 메서드 사용
+  blackPink.sayMembersCount();
+}
