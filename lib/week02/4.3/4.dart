9f6dcc6a6fb943b83723fe5ae871a02d135d083d
lellon0403 <a67733198@gmail.com>
2025-09-14 17:11:49 +0900

    feat(week02/4.3): Add example using wildcard pattern matching with a guard.

---
+++ lib/week02/4.3/4.dart
@@ -0,0 +1,14 @@
+void main() {
+  (int a, int b) val = (1,=1);
+
+  //default 가 출력됩니다. 만약에 b 값을 0 이상으로 변경하면
+  //1,_ 를 출력할 수 있습니다.
+  switch(val) {
+    case (1,_) when val.$2 > 0:
+      print('1,_');
+      break;
+
+    default:
+      print('default');
+  }
+}
