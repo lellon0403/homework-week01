3b578b1ae79e59e8b23bd5287f57876701021b67
lellon0403 <a67733198@gmail.com>
2025-09-14 17:05:49 +0900

    feat(week02): Implement switch statement example with pattern matching

--- lib/week02/4.3/2.dart
+++ lib/week02/4.3/2.dart
@@ -1,1 +1,47 @@
-void switcher(dynamic anything) {}
+void switcher(dynamic anything) {
+  switch (anything) {
+    //정확히 'aaa' 문자열만 매치합니다.
+    case 'aaa':
+      print('match : aaa');
+      break;
+
+    //정확히 [1,2] 리스트만 매치합니다.
+
+    case [1, 2]:
+      print('match : [1,2]');
+      break;
+
+    // 3개의 값이 들어 있는 리스트를 모두 매치합니다.
+    case [_, _, _]:
+      print('match[_,_,_]');
+      break;
+
+    //첫 번째와 두 번째 값에 int가 입력된 리스트를 매치합니다.
+    case [int a, int b]:
+      print('match : [int $a, int $b]');
+      break;
+
+    //첫 번째 값에 String, 두 번째 값에 int 가 입력된 Record 타입을 매치합니다.
+    case (String a, int b):
+      print('match : (String : $a, int: $b)');
+      break;
+
+    //아무것도 매치되지 않을 경우 실행합니다.
+    default:
+      print('no match');
+  }
+}
+
+void main() {
+  // match: aaa출력
+  switcher('aaa');
+
+  // match : [1,2] 출력
+  switcher([1, 2]);
+
+  // match : [_,_,_] 출력
+  switcher(3, 4, 5);
+
+  // match : [int 6, int 7] 출력
+  switcher()
+}
