70712f8117bff59301b93d3b1ccea21bf1e9cfd2
lellon0403 <a67733198@gmail.com>
2025-09-14 12:25:11 +0900

    Fix: Correct BoyGroup class inheritance and constructor.

--- lib/week02/2.3/1_inheritance.dart
+++ lib/week02/2.3/1_inheritance.dart
@@ -15,14 +15,14 @@ class Idol {
 
 //extends 키워드를 사용해서 상속받습니다.
 //class 자식 클래스 extends 부모 클래스 순서입니다.
-class BoyGroup extends Idol() {
-
+class BoyGroup extends Idol {
   //상속받은 생성자
-  BoyGroup(
-    String name,
-    int membersCount,) : super(// super는 부모 클래스를 지칭합니다.
-    name,membersCount,
-    );
+  BoyGroup(String name, int membersCount)
+    : super(
+        // super는 부모 클래스를 지칭합니다.
+        name,
+        membersCount,
+      );
 
   //상속받지 않은 기능
   void sayMale() {
@@ -31,6 +31,5 @@ class BoyGroup extends Idol() {
 }
 
 void main() {
-
-  BoyGroup bts = BoyGroup('BTS',7);
+  BoyGroup bts = BoyGroup('BTS', 7);
 }
