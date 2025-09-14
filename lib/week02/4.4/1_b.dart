c043b6f646424114e9f6a29f78ea68c1c97c39a0
lellon0403 <a67733198@gmail.com>
2025-09-14 17:16:19 +0900

    Fix: Implement Child class correctly and add Child2 example

--- lib/week02/4.4/1_b.dart
+++ lib/week02/4.4/1_b.dart
@@ -4,6 +4,11 @@ import '1_a.dart';
 Parent parent = Parent();
 
 // 가능
-base class Child extends Parent {
-  //s
-}
+base class Child extends Parent {}
+
+// subtype of base or final is not base final or sealed 에러가 발생합니다.
+// base / sealed / final 제한자 중 하나가 필요합니다.
+
+class Child2 extends Parent {}
+
+//
