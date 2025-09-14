59410d6fc47cbdcc8534b58e555cf46d1d92c7be
lellon0403 <a67733198@gmail.com>
2025-09-14 12:41:01 +0900

    feat: Implement Idol class and implement it with GirlGroup

--- lib/week02/2.5/1_interface.dart
+++ lib/week02/2.5/1_interface.dart
@@ -1,3 +1,18 @@
+class Idol {
+  final String name;
+  final int membersCount;
+
+  Idol(this.name, this.membersCount);
+
+  void sayName() {
+    print('저는 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명 입니다.');
+  }
+}
+
 // implements 키워드를 사용하면 원하는 클래스를 인터페이스로 사용할 수 있습니다.
 class GirlGroup implements Idol {
   final String name;
