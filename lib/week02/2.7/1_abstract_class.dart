5576fc7538e92b4fd43ed5e808c6953914717025
lellon0403 <a67733198@gmail.com>
2025-09-14 13:28:28 +0900

    feat: Implement abstract class example with `GirlGroup` class

--- lib/week02/2.7/1_abstract_class.dart
+++ lib/week02/2.7/1_abstract_class.dart
@@ -4,4 +4,30 @@ abstract class Idol {
   final int membersCount;
 
   Idol(this.name, this.membersCount); //생성자 선언
+
+  void sayName(); //추상 메서드 선언
+  void sayMembersCount(); //추상 메서드 선언
+}
+
+//implements 키워드를 사용해 추상 클래스를 구현하는 클래스
+class GirlGroup implements Idol {
+  final String name;
+  final int membersCount;
+
+  GirlGroup(this.name, this.membersCount);
+
+  void sayName() {
+    print('저는 여자 아이돌 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명 입니다.');
+  }
+}
+
+void main() {
+  GirlGroup blackPink = GirlGroup('블랙핑크', 4);
+
+  blackPink.sayName();
+  blackPink.sayMembersCount();
 }
