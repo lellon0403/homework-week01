a7d07612773543209bc735c2552af9eb8d339cb9
lellon0403 <a67733198@gmail.com>
2025-09-14 17:18:19 +0900

    Fix: Prevent `Child3` from implementing `Parent`

--- lib/week02/4.4/1_b.dart
+++ lib/week02/4.4/1_b.dart
@@ -11,4 +11,6 @@ base class Child extends Parent {}
 
 class Child2 extends Parent {}
 
-//
+//subtype of base or final is not base final or sealed 에러가 발생합니다.
+//base 클래스는 implement가 불가능합니다.
+class Child3 implements Parent {}
