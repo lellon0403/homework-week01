d84b7d712a705733fad62bd39b1aa68407bfc084
lellon0403 <a67733198@gmail.com>
2025-09-14 16:07:18 +0900

    Fix: Use cascade operator correctly in Idol example

--- lib/week02/2.10/1_cascade_operator.dart
+++ lib/week02/2.10/1_cascade_operator.dart
@@ -16,8 +16,7 @@ class Idol {
 void main() {
   //cascade operator (...)을 사용하면
   // 선언한 변수의 메서드를 연속으로 실행할 수 있습니다.
-  Idol blackpink = Idol('블랙핑크', 4);
-
+  Idol blackpink = Idol('블랙핑크', 4)
   ...sayName();
-  blackpink.sayMembersCount();
+  ...sayMembersCount();
 }
