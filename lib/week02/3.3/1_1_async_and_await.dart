902b440ca416061bdb3ba166cae9101c301b7bf6
lellon0403 <a67733198@gmail.com>
2025-09-14 16:18:18 +0900

    feat(week02): Implement basic async/await example

--- lib/week02/3.3/1_1_async_and_await.dart
+++ lib/week02/3.3/1_1_async_and_await.dart
@@ -3,4 +3,13 @@ void main() {
 }
 
 // async 키워드는 함수 매개변수 정의와 바디 사이에 입력합니다.
-Future<void> addNumbers(int number1, int number2) async {}
+Future<void> addNumbers(int number1, int number2) async {
+  print('$number1 + $number2 계산 시작!');
+
+  //await는 대기하고 싶은 비동기 함수 앞에 입력합니다.
+  await Future.delayed(Duration(seconds: 3), () {
+    print('$number1 + $number2 = ${number1 + number2}');
+  });
+
+  print('$number1 + $number2 코드 실행 끝');
+}
