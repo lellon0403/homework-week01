60a2527146df9d3b166d5ce155b5d099cb839b39
lellon0403 <a67733198@gmail.com>
2025-09-14 17:10:18 +0900

    feat(week02/4.3): Implement nullable boolean switch statement example

---
+++ lib/week02/4.3/3.dart
@@ -0,0 +1,15 @@
+void main() {
+  //val 에 입력될 수 있는 값은 true,false,null입니다.
+  bool? val;
+
+  //null 조건을 입력하지 않았기 때문에 non exhaustive switch statment 에러가 발생합
+  //니다. null case를 추가하거나 default case를 추가해야 에러가 사라집니다.
+  switch (val) {
+    case true:
+      print('true');
+
+    case false:
+      print('false');
+  }
+  ;
+}
