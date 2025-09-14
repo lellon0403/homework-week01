5b98a9ab058bd38333f7a165cf67432f60db2129
lellon0403 <a67733198@gmail.com>
2025-09-14 17:38:19 +0900

    Fix: Corrected syntax error in addTwoNumbers function

--- lib/week01/1.7/1_2_function.dart
+++ lib/week01/1.7/1_2_function.dart
@@ -1,11 +1,7 @@
-int addTwoNumbers({
-  required int a,
-  required int b
-}); {
-  return a+ b;
+int addTwoNumbers({required int a, required int b}) {
+  return a + b;
 }
 
 void main() {
-  print(addTwoNumbers(a:1,b:2));
+  print(addTwoNumbers(a: 1, b: 2));
 }
-
