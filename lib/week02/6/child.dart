e9c6809ab5cf9cc7fb066f426c5e9a7e3fe50adc
lellon0403 <a67733198@gmail.com>
2025-09-21 22:03:37 +0900

    feat: Add basic 'Code Factory' text widget example

---
+++ lib/week02/6/child.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body : Center(
+
+          //하나의 위젯만 가운데 정렬 가능
+          child: Text('Code Factory'),
+        )
+      )
+    )
+  );
+}
