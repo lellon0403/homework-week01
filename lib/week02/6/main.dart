03f36ae73e9f419b0c7c10d077fbc309fb3d5820
lellon0403 <a67733198@gmail.com>
2025-09-21 22:08:37 +0900

    feat: Implement basic Flutter app structure

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -1,1 +1,19 @@
-import 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body : Center(
+          child : // 여기에 예제 코드 작성하기
+        )
+        ,
+        )
+    )
+  }
+}
