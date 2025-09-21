7c78f344a2f049129d3ade46bd4a6229d830f1db
lellon0403 <a67733198@gmail.com>
2025-09-22 00:01:26 +0900

    feat: Implement basic splash screen widget

---
+++ lib/week03/splash_screen/main_01.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen()); // SplashScreen 위젯을 첫 화면으로 지정
+}
+
+class SplashScreen extends StatelessWidget { //statelessWidget 선언
+  @override
+  Widget build(BuildContext context) {  // 위젯의 UI 구현
+     
+  }
+}
