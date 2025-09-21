f2e2add573cc00a718ba514fbf29b14e451a0ae8
lellon0403 <a67733198@gmail.com>
2025-09-21 21:57:37 +0900

    feat(week02/5): Center the 'Hello Code Factory' text

--- lib/week02/5/main.dart
+++ lib/week02/5/main.dart
@@ -7,8 +7,10 @@ void main(){
   runApp(
     MaterialApp(  //머티리얼 디자인 위젯
       home: Scaffold( //Scaffold 위젯
-        body: Text(   //Text 위젯
+        body: Center(
+          child: Text(   //Text 위젯
           'Hello Code Factory', //  마지막 매개변수 끝에 콤마 추가
+        ),
         ),
       ),
     ),
