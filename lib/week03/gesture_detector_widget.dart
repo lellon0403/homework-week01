e15edac2c5bca0ade7490f1ced3c229af885a4d0
lellon0403 <a67733198@gmail.com>
2025-09-21 22:28:38 +0900

    feat(week03): Implement basic GestureDetector example

--- lib/week03/gesture_detector_widget.dart
+++ lib/week03/gesture_detector_widget.dart
@@ -10,7 +10,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body : Center(
-          child : // 여기에 예제 코드 작성하기
+          child : GestureDetector(
+            //한 번 탭했을 때 실행할 함수
+            onTap: () {
+              //출력 결과는 안드로이드 스튜디오의 [Run] 탭에서 확인 가능합니다.
+              print('on tap');
+            },
+            //두 번 탭했을 때 실행할 함수
+            onDoubleTap: (){
+              print('on double tap');
+            },
+            //길게 눌렀을 때 실행할 함수
+            onLongPress: () {
+              print('on long press');
+            },
+            //제스처를 적용할 위젯
+            child: Container(
+              decoration: BoxDecoration(
+                color:Colors.red,
+              ),
+              width: 100.0,
+              height: 100.0,
+            ),
+          ),
         ),
        ),
     );
