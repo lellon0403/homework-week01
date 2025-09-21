f80926f5a687f485a11d40b1ebc2e545c9f44734
lellon0403 <a67733198@gmail.com>
2025-09-22 00:20:56 +0900

    feat(splash): Replace empty Container with Scaffold in SplashScreen

--- lib/week03/splash_screen/main_06.dart
+++ lib/week03/splash_screen/main_06.dart
@@ -9,7 +9,7 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner : false,
-      home: Container()
+      home: Scaffold()
     )
   }
 }
