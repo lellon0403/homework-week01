115680ee1bb1f75dd7012afc7d068f2570380943
lellon0403 <a67733198@gmail.com>
2025-09-22 00:35:58 +0900

    feat: Display web content in HomeScreen using WebViewWidget

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -24,7 +24,9 @@ class HomeScreen extends StatelessWidget {
         //가운데 정렬
         centerTitle: true,
       ),
-      body: Text('HomeScreen'),
+      body: WebViewWidget(  //웹뷰 위젯 추가하기 (에러 잠시 무시하기)
+        controller: webViewController, //에러 발생
+      ),
     );
   }
 }
