8b50b3f638db15b552494ff82e33eb761510a1c3
lellon0403 <a67733198@gmail.com>
2025-09-21 22:51:38 +0900

    feat: Implement basic Row widget example

--- lib/week03/row_widget.dart
+++ lib/week03/row_widget.dart
@@ -6,8 +6,29 @@ void main() {
 
 class RowWidgetExample extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body :  SizedBox(
+          height: double.infinity,
+          child: Row(
+
+            //주축 정렬 지정
+            mainAxisAlignment: MainAxisAlignment.start,
+            //반대축 정렬 지정
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            //넣고 싶은 위젯 입력
+            children: [
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.red,
+              ),
+              // s
+            ]
+          )
+        ),)
+    )
   }
 }
