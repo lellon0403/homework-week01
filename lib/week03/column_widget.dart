6c0cd426f8192590a1578df51c1d1bd608877152
lellon0403 <a67733198@gmail.com>
2025-09-21 23:00:38 +0900

    feat: Add basic Column widget example with alignment and spacing

--- lib/week03/column_widget.dart
+++ lib/week03/column_widget.dart
@@ -15,11 +15,39 @@ class ColumnWidgetExample extends StatelessWidget {
 
           //반대축에서 이동할 공간을 제공해주기 위해 너비를 최대한으로 설정
           width: double.infinity,
-          child: Column(children: [
+          child: Column(
             
-          ],),
-        )
-      )
-    )
+            //주축 정렬 지정
+            mainAxisAlignment: MainAxisAlignment.start,
+
+            //반대축 정렬 지정
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            // 넣고 싶은 위젯 입력
+            children: [ 
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.red,
+              ),
+
+              //SizedBox는 일반적으로 공백을 생성할 때 사용
+              const SizedBox(width:12.0),            
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.green,
+              ),
+              const SizedBox(width:12.0),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color:Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
