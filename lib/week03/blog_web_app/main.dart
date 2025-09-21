81bc1bb150e95578bb482864b903ef3d64ec3f3a
lellon0403 <a67733198@gmail.com>
2025-09-22 00:30:58 +0900

    feat: Implement basic home screen and app entry point

--- lib/week03/blog_web_app/main.dart
+++ lib/week03/blog_web_app/main.dart
@@ -1,1 +1,11 @@
-import 'package:myapp'
+import 'package:myapp/week03/blog_web_app/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+      ),
+  );
+}
